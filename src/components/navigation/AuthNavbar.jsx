0e8c39daff28b0583cd831dda0ee33d476f7b0d6
enes <ennesvarol480@gmail.com>
2026-01-03 18:55:09 +0300

    removed authnavbar component

--- src/components/navigation/AuthNavbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AuthNavbar() {
-  return (
-    <div>AuthNavbar</div>
-  )
-}
-
-export default AuthNavbar
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom"
-import AuthNavbar from "../components/navigation/AuthNavbar"
 import React from 'react'
 
 function AuthLayout() {
   return (
     <div>
-      <AuthNavbar/>
       <Outlet/>
     </div>
   )
