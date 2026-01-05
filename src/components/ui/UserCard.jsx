e8ebc279b86c1fed5aefda13f3b2de0020c7685f
enes <ennesvarol480@gmail.com>
2026-01-05 22:21:00 +0300

    feat(ui): add UserCard component

---
+++ src/components/ui/UserCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function UserCard({user,onRemove,onEdit}) {
+  return (
+    <div className="user-card">
+      <div className="user-info">
+        <h2>{user.id}</h2>
+        <span>-{user.firstName}</span>
+        <span>-{user.secondName}</span>
+        <span>-{user.age}</span>
+        <span>-{user.email}</span>
+        <span>-{user.location}</span>
+      </div>
+      <div className="user-edit">
+        <button onClick={() => onRemove(user.id)}>Remove</button>
+        <button onClick={() => onEdit(user.id)}>Edit</button>
+      </div>
+    </div>
+  );
+}
+
+export default UserCard;
