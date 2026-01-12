683c6a1b21aff08ed2942b86a4942384f30277d3
enes <ennesvarol480@gmail.com>
2026-01-12 19:54:58 +0300

    refactor(product service): add try-catch error handling to requests

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,9 +1,19 @@
 const BASE_URL = "http://localhost:4001/products";
 
-export const getProducts = () => fetch(BASE_URL).then((res) => res.json());
+export const getProducts = async () => {
+  const res = await fetch(BASE_URL)
+  if (!res.ok){
+    throw new Error("Failed to fetch products")
+  }
+  return res.json();
+}
 
 export const deleteProduct = async (id) => {
-  await fetch(`${BASE_URL}/${id}`, {
+  const res = await fetch(`${BASE_URL}/${id}`, {
     method: "DELETE",
   });
+  if (!res.ok) {
+    throw new Error("Failed to delete product");
+  }
+  return res.json();
 };
